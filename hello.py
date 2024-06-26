4fff6af1d82c776676561ede9e05fc175bd79410
KiselyovDmitriy <simon-000@mail.ru>
2024-06-26 19:28:20 +0300

    Методы строк

---
+++ hello.py
@@ -0,0 +1,9 @@
+my_string = input()
+
+print("Строка в верхнем регистре: ", my_string.upper())
+print("Строка в нижнем регистре: ", my_string.lower())
+print("Строка без пробелов: ", my_string.replace(' ', ''))
+print("Первый символ строки: ", my_string[0])
+print("Последний символ строки: ", my_string[-1])
+print("Длина введенного текста:", len(my_string))
+
